0e1b7facd4be7a17b9e2af6551a7a174bffcc94d
aleja <alejandrovasquezesc@gmail.com>
2026-08-06 11:46:29 -0600

    chore(ui): reduce toast display duration

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 import { Toaster } from 'sonner';
+import { ConnectionOverlay } from './components/common';
 
 export const App = () => {
     return (
@@ -9,12 +10,14 @@ export const App = () => {
                 position="top-right"
                 richColors
                 closeButton
+                duration={3000}
                 toastOptions={{
                     style: {
                         marginTop: '73px',
                     },
                 }}
             />
+            <ConnectionOverlay />
             <RouterProvider router={router} />
         </>
     );
